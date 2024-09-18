dc25561c870135d9513bba6cd0e0d9309da3cdee
reddytharun <reddytharun367@gmail.com>
2024-09-18 13:01:02 +0530

    core practice

---
+++ src/student/StudentInformation.java
@@ -0,0 +1,15 @@
+package student;
+
+public class StudentInformation {
+    public String studentName;
+    public String rollNumber;
+    public String fatherName;
+
+    public static void main(String[] args) {
+        StudentInformation studentInformation = new StudentInformation();
+        studentInformation.studentName="arun";
+        studentInformation.rollNumber="77";
+        studentInformation.fatherName="kiran";
+        System.out.println(" student Details  : " + studentInformation.studentName);
+    }
+}

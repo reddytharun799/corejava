c718b66628314833a486c22e6c0eebb36427c909
reddytharun <reddytharun367@gmail.com>
2024-09-21 11:36:26 +0530

    4 core

--- src/student/StudentInformation.java
+++ src/student/StudentInformation.java
@@ -10,6 +10,9 @@ public class StudentInformation {
         studentInformation.studentName="arun";
         studentInformation.rollNumber="77";
         studentInformation.fatherName="kiran";
+        System.out.println(" student Details  : " + studentInformation.studentName);
+        System.out.println(" student Details  : " + studentInformation.rollNumber);
+        System.out.println(" student Details  : " + studentInformation.fatherName);
         System.out.println(" student Details  : " + studentInformation.studentName+"  "+
 
                 studentInformation.rollNumber+"  "+studentInformation.fatherName);

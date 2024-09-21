78d1b03e62371795f081ed2c7d6d28c5261aced7
reddytharun <reddytharun367@gmail.com>
2024-09-21 12:00:54 +0530

    only once 6

--- src/student/StudentInformation.java
+++ src/student/StudentInformation.java
@@ -1,23 +1,4 @@
 package student;
 
-public class StudentInformation {
-    public String studentName;
-    public String rollNumber;
-    public String fatherName;
 
-    public static void main(String[] args) {
-        StudentInformation studentInformation = new StudentInformation();
-        studentInformation.studentName="arun";
-        studentInformation.rollNumber="77";
-        studentInformation.fatherName="kiran";
-        System.out.println(" student Details  : " + studentInformation.studentName);
-        System.out.println(" student Details  : " + studentInformation.rollNumber);
-        System.out.println(" student Details  : " + studentInformation.fatherName);
-        System.out.println(" student Details  : " + studentInformation.rollNumber);
-        System.out.println(" student Details  : " + studentInformation.fatherName);
-        System.out.println(" student Details  : " + studentInformation.studentName+"  "+
-
-                studentInformation.rollNumber+"  "+studentInformation.fatherName);
-    }
-}
 tharun reddy 1
